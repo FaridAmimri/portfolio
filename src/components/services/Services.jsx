f97beaba1a0377325e08da7a09cabaab77f24b2a
Farid Amimri <farid.amimri@gmail.com>
2023-11-06 12:02:34 +0100

    Add useInView animation with framer motion

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,37 +1,68 @@
 /** @format */
 
+import { useRef } from 'react'
 import './services.scss'
-import { motion } from 'framer-motion'
+import { motion, useInView } from 'framer-motion'
 
 const services = [
   {
     id: '1',
     title: 'Branding',
     description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-velit et placeat repellat, non id adipisci expedita rerum delectus
-minus in vel.`
+velit et placeat repellat.`
   },
   {
     id: '2',
     title: 'Branding',
     description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-velit et placeat repellat, non id adipisci expedita rerum delectus
-minus in vel.`
+    velit et placeat repellat.`
   },
   {
     id: '3',
     title: 'Branding',
     description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-velit et placeat repellat, non id adipisci expedita rerum delectus
-minus in vel.`
+    velit et placeat repellat.`
+  },
+  {
+    id: '4',
+    title: 'Branding',
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+    velit et placeat repellat.`
   }
 ]
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1
+    }
+  }
+}
+
 const Services = () => {
+  const ref = useRef()
+
+  const isInView = useInView(ref, { margin: '-100px' })
+
   return (
-    <motion.div className='services'>
+    <motion.div
+      className='services'
+      variants={variants}
+      initial='initial'
+      ref={ref}
+      animate={isInView && 'animate'}
+    >
       {/* Text Container */}
-      <motion.div className='textContainer'>
+      <motion.div className='textContainer' variants={variants}>
         <p>
           I focus on helping your brand grow
           <br />
@@ -40,22 +71,23 @@ const Services = () => {
         <hr />
       </motion.div>
       {/* Title Container */}
-      <motion.div className='titleContainer'>
+      <motion.div className='titleContainer' variants={variants}>
         <div className='title'>
           <img src='/work.png' alt='people' />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{ color: 'orange' }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className='title'>
           <h1>
-            <b>For Your</b> Business.
+            <motion.b whileHover={{ color: 'orange' }}>For Your</motion.b>{' '}
+            Business.
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
       {/* List Container */}
-      <motion.div className='listContainer'>
+      <motion.div className='listContainer' variants={variants}>
         {services.map((service) => (
           <motion.div
             key={service.id}
