0a59aee46fe59d24b2be61a2a7a3d6a030b25036
Farid Amimri <farid.amimri@gmail.com>
2023-11-07 14:30:55 +0100

    Design Project Card component using framer motion

---
+++ src/components/projectCard/ProjectCard.jsx
@@ -0,0 +1,37 @@
+/** @format */
+
+import './projectCard.scss'
+import { useRef } from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
+const ProjectCard = ({ item }) => {
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({
+    target: ref
+    // offset: ['start start', 'end start']
+  })
+
+  const y = useTransform(scrollYProgress, [0, 1], [-100, 100])
+
+  return (
+    <section>
+      <div className='container'>
+        <div className='wrapper'>
+          <div className='imageContainer' ref={ref}>
+            <img src={item.img} alt={item.title} />
+          </div>
+          <motion.div className='textContainer' style={{ y: y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <a href={item.link} target='_blank'>
+              Voir Demo
+            </a>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProjectCard
