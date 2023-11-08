d5713ecf271ad4e3bac5566392da5656ea354d6e
Farid Amimri <farid.amimri@gmail.com>
2023-11-08 16:17:59 +0100

    Handle Email Submit Form  using EmailJs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,10 @@
 /** @format */
 
-import { useRef } from 'react'
 import './contact.scss'
+import { useRef, useState } from 'react'
 import { motion, useInView } from 'framer-motion'
+import emailjs from '@emailjs/browser'
+import { Oval } from 'react-loader-spinner'
 
 const variants = {
   initial: {
@@ -21,12 +23,43 @@ const variants = {
 
 const Contact = () => {
   const ref = useRef()
+  const formRef = useRef()
 
   const isInView = useInView(ref, { margin: '-100px' })
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs
+      .sendForm(
+        'service_xyl4vfy',
+        'template_ueij0ii',
+        formRef.current,
+        'dxK4eOAw61JXwWe-i'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          setSuccess(true)
+          setLoading(false)
+        },
+        (error) => {
+          console.log(error.text)
+          setError(true)
+          setLoading(false)
+        }
+      )
+  }
 
   return (
     <motion.div
       ref={ref}
+      onSubmit={handleSubmit}
       className='contact'
       variants={variants}
       initial='initial'
@@ -35,19 +68,69 @@ const Contact = () => {
       {/* Contact Info */}
       <motion.div className='textContainer' variants={variants}>
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
+
         <motion.div className='details' variants={variants}>
-          <h2>Mail</h2>
-          <span>contact@faridigital.fr</span>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75'
+            />
+          </svg>
+          <a href='mailto: contact@faridigital.fr'>
+            <span> contact@faridigital.fr</span>
+          </a>
         </motion.div>
+
         <motion.div className='details' variants={variants}>
-          <h2>Address</h2>
-          <span>94240 Stains, France</span>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
+            />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
+            />
+          </svg>
+          <span>93240 Stains, France</span>
         </motion.div>
+
         <motion.div className='details' variants={variants}>
-          <h2>Phone</h2>
-          <span>+33 6 60 40 82 05</span>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z'
+            />
+          </svg>
+          <span>+33 6 40 40 82 05</span>
         </motion.div>
       </motion.div>
+
       {/* Contact Form */}
       <div className='formContainer'>
         <motion.div
@@ -79,15 +162,37 @@ const Contact = () => {
             />
           </svg>
         </motion.div>
+
         <motion.form
+          ref={formRef}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 4, duration: 1 }}
         >
-          <input type='text' required placeholder='Name' />
-          <input type='email' required placeholder='Email' />
-          <textarea rows={8} placeholder='Message'></textarea>
+          <input type='text' required placeholder='Name' name='name' />
+          <input type='email' required placeholder='Email' name='email' />
+          <textarea rows={8} placeholder='Message' name='message' />
           <button>Envoyer</button>
+          <div className='statusContainer'>
+            {loading && (
+              <Oval
+                height={30}
+                width={30}
+                color='orange'
+                wrapperStyle={{}}
+                wrapperClass=''
+                visible={true}
+                ariaLabel='oval-loading'
+                secondaryColor='gray'
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+              />
+            )}
+            {success && <span>Votre message a bien été envoyé.</span>}
+            {error && (
+              <span>Une erreur s’est produite, veuillez réessayer. </span>
+            )}
+          </div>
         </motion.form>
       </div>
     </motion.div>
