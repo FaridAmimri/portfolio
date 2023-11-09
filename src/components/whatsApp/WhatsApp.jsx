5d1725bdc69d7ead63a0c8d7a2e5b001c2092634
Farid Amimri <farid.amimri@gmail.com>
2023-11-09 12:14:11 +0100

    Add WhatsApp Button

---
+++ src/components/whatsApp/WhatsApp.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import './whatsApp.scss'
+import { FaWhatsapp } from 'react-icons/fa'
+
+const WhatsApp = () => {
+  return (
+    <div className='whatsapp'>
+      <a href='https://wa.me/33640408205' target='_blank'>
+        <FaWhatsapp />
+      </a>
+    </div>
+  )
+}
+
+export default WhatsApp
